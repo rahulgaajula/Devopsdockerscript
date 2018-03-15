2a2ef8f3e667665dde701f4b971e9ad37193e6a3
rahul <rahul.gajula40@gmail.com>
2018-03-15 17:28:33 -0400

    updated launch_script.sh

--- launch_script.sh
+++ launch_script.sh
@@ -11,7 +11,7 @@ echo "--------------------------------------------------------"
 rm -rf launcher
 # Clone Rancher Catalog of <Module> from gitlab
 echo "Cloning Rancher catalog of provided DIWO service"
-git clone -b dev http://rahulgaajula:We!come123@github.com/rahulgaajula/launcher.git
+git clone -b master http://rahulgaajula:We!come123@github.com/rahulgaajula/launcher.git
 echo "--------------------------------------------------------"
 
 
@@ -22,18 +22,18 @@ echo "--------------------------------------------------------"
 
 # Remove rancher service of <Module>
 echo "Removing existing service from Rancher........."
-echo $7 | $3/rancher-compose --url $4/v1/ --access-key $5 --secret-key $6 --project-name $9 --verbose rm
+echo $7 | $3/rancher-compose --url $4/v1/ --access-key $5 --secret-key $6 --project-name PRR_SN --verbose rm
 echo "--------------------------------------------------------"
 sleep 15
 # Create rancher service of <Module>
 echo "Creating given DIWO service in Rancher........."
-echo $7 | $3/rancher-compose --url $4/v1/ --access-key $5 --secret-key $6 --project-name $9 --verbose create
+echo $7 | $3/rancher-compose --url $4/v1/ --access-key $5 --secret-key $6 --project-name PRR_SN --verbose create
 echo "--------------------------------------------------------"
 sleep 15
 
 # Start rancher service of <Module>
 echo "Starting created DIWO service in Rancher..........."
-echo $7 | $3/rancher-compose --url $4/v1/ --access-key $5 --secret-key $6 --project-name $9 --verbose start
+echo $7 | $3/rancher-compose --url $4/v1/ --access-key $5 --secret-key $6 --project-name PRR_SN --verbose start
 echo "--------------------------------------------------------"
 
 cd ../../../.. && rm -rf launcher
